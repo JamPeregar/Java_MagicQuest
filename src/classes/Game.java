500b678b2938ea89a683f353a7ace49aa3452afe
RinKirsan <116436137+RinKirsan@users.noreply.github.com>
2024-05-10 21:17:04 +0400

    Update Game.java

--- src/classes/Game.java
+++ src/classes/Game.java
@@ -14,18 +14,18 @@ public class Game {
 
         Scanner scanner = new Scanner(System.in);
 
-        Location currentLocation = start;
+        Location currentLocation = start;//стартовая локация
         System.out.println(currentLocation);
         while (true) {
             System.out.println("Введите команду 'перейти', чтобы выбрать локацию для перехода:");
             String command = scanner.nextLine();
-            if (command.equalsIgnoreCase("перейти")) {
+            if (command.equalsIgnoreCase("перейти")) {//команда для перехода
                 System.out.println("Доступные локации для перехода:");
                 for (String direction : currentLocation.getExits().keySet()) {
                     System.out.println(direction);
                 }
                 System.out.println("Введите название локации для перехода:");
-                String chosenLocation = capitalize(scanner.nextLine());
+                String chosenLocation = capitalize(scanner.nextLine().toLowerCase());
                 Location nextLocation = currentLocation.go(chosenLocation);
                 if (nextLocation != null) {
                     currentLocation = nextLocation;
@@ -34,25 +34,25 @@ public class Game {
                 } else {
                     System.out.println("Неверное название локации.");
                 }
-            } else {
+            } else {//остальной ввод
                 System.out.println("Неверная команда. Попробуйте снова.");
             }
 
         }
     }
-    public void creationLocations (){
+    private void creationLocations (){
         start = new Location("Начальная локация", "Вы находитесь в начальной точке");
         garden = new Location("Сад",
                 "Вы находитесь в уютном саду, окруженном цветущими растениями и ароматными цветами. Птицы щебечут на деревьях, а солнечные лучи проникают сквозь листву, создавая игривые тени на земле");
 
     }
 
-    public void createTransitions (){
+    private void createTransitions (){
         start.addExit("Сад", garden);
         garden.addExit("Начальная локация", start);
     }
 
-    public static String capitalize(String inputString) {
+    private static String capitalize(String inputString) {
         if (inputString == null || inputString.isEmpty()) {
             return inputString;
         }

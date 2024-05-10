0858b8c9419e0c7fd64f2522aae56d5e46b23284
RinKirsan <116436137+RinKirsan@users.noreply.github.com>
2024-05-10 23:37:35 +0400

    Update Game.java

--- src/classes/Game.java
+++ src/classes/Game.java
@@ -5,37 +5,76 @@ public class Game {
 
     private Location start;
     private Location garden;
+    private Player p1;
+    private Player p2;
 
     //Controls game, catch player commands
     public void game(){
 
+        createPlayers();
         creationLocations();
         createTransitions();
+        createItems();
 
         Scanner scanner = new Scanner(System.in);
 
         Location currentLocation = start;//стартовая локация
         System.out.println(currentLocation);
         while (true) {
-            System.out.println("Введите команду 'перейти', чтобы выбрать локацию для перехода:");
+            System.out.println("""
+                    \nСписок доступных команд:
+                    перейти - (переход в другую локацию)
+                    инвентарь - (показывает ваш инвентарь)
+                    подобрать - (подобрать предмет)
+                    использовать - (использовать предмет)""");
+
             String command = scanner.nextLine();
-            if (command.equalsIgnoreCase("перейти")) {//команда для перехода
-                System.out.println("Доступные локации для перехода:");
-                for (String direction : currentLocation.getExits().keySet()) {
-                    System.out.println(direction);
-                }
-                System.out.println("Введите название локации для перехода:");
-                String chosenLocation = capitalize(scanner.nextLine().toLowerCase());
-                Location nextLocation = currentLocation.go(chosenLocation);
-                if (nextLocation != null) {
-                    currentLocation = nextLocation;
-                    System.out.println("Вы прибыли в " + currentLocation.getName());
-                    System.out.println(currentLocation.getDescription());
-                } else {
-                    System.out.println("Неверное название локации.");
-                }
-            } else {//остальной ввод
-                System.out.println("Неверная команда. Попробуйте снова.");
+            switch (capitalize(command.toLowerCase())){
+                case "Перейти":
+                    System.out.println("Доступные локации для перехода:");
+                    for (String direction : currentLocation.getExits().keySet()) {
+                        System.out.println(direction);
+                    }
+                    System.out.println("Введите название локации для перехода:");
+                    String chosenLocation = capitalize(scanner.nextLine().toLowerCase());
+                    Location nextLocation = currentLocation.go(chosenLocation);
+                    if (nextLocation != null) {
+                        currentLocation = nextLocation;
+                        System.out.println("Вы прибыли в " + currentLocation.getName());
+                        System.out.println(currentLocation.getDescription());
+                    } else {
+                        System.out.println("Неверное название локации.");
+                    }
+
+                    break;
+                case "Инвентарь":
+                    p1.showInventory();
+                    break;
+
+                case "Подобрать":
+                    System.out.println("Доступные предметы для подбора:");
+                    for (AbstractItem item : currentLocation.getLocalInventory()) {
+                        System.out.println(item.getName());
+                    }
+                    System.out.println("Введите название предмета, который хотите подобрать:");
+                    String itemName = capitalize(scanner.nextLine().toLowerCase());
+                    AbstractItem pickedItem = currentLocation.takeItem(itemName);
+                    if (pickedItem != null) {
+                        p1.giveItem(pickedItem);
+                        System.out.println("Вы подобрали: " + pickedItem.getName());
+                    } else {
+                        System.out.println("Такого предмета нет в локации.");
+                    }
+                    break;
+
+                case "Использовать":
+                    // Код для использования предмета
+                    p1.showInventory();
+                    System.out.println("Введите название предмета, который хотите использовать:");
+                    String itemNameToUse = capitalize(scanner.nextLine().toLowerCase());
+                    p1.useItem(itemNameToUse);
+                    break;
+
             }
 
         }
@@ -50,6 +89,19 @@ public class Game {
     private void createTransitions (){
         start.addExit("Сад", garden);
         garden.addExit("Начальная локация", start);
+    }
+
+    private void createItems (){
+        AbstractItem potion = new Potion("Странное зелье", "Загадочный напиток, горький запах");
+        AbstractItem water = new Water("Пузырёк воды", "H2o или просто вода, не забывайте увлажнять организм!");
+        AbstractItem water2 = new Water("Пузырёк воды", "H2o или просто вода, не забывайте увлажнять организм!");
+        start.addItem(potion);
+        start.addItem(water);
+    }
+
+    private void createPlayers(){
+        p1 = Player.createPlayer("Nik", "Student");
+        p2 = Player.createPlayer("Rin", "Student");
     }
 
     private static String capitalize(String inputString) {

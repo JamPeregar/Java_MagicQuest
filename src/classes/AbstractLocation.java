d01f1f633de51683877ce617cd1bf96273923a2c
RinKirsan <116436137+RinKirsan@users.noreply.github.com>
2024-05-10 15:12:23 +0400

    Update AbstractLocation.java

--- src/classes/AbstractLocation.java
+++ src/classes/AbstractLocation.java
@@ -3,11 +3,23 @@ package classes;
 public class AbstractLocation {
     private final String name;
     private final String descr;
-    private String[] paths;
+    //private String[] paths;
 
     public AbstractLocation(final String name, final String dscr, String[] paths) {
         this.name = name;
         this.descr = dscr;
+    }
+    public AbstractLocation(final String name, final String dscr) {
+        this.name = name;
+        this.descr = dscr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return descr;
     }
 
 

44db7b063d4fd0e3d1d7893b622f3cdd6ec7e1ba
JamPeregar <108716574+JamPeregar@users.noreply.github.com>
2024-05-22 22:17:20 +0400

    Update Furniture.java
    Бесполезное переписывание метода обмена

--- src/classes/Furniture.java
+++ src/classes/Furniture.java
@@ -13,30 +13,7 @@ public class Furniture extends Entity implements Interractable {
     public Furniture(String name, String descr) {
         super(name, descr);
     }
-
-    @Override
-    public void trade(Entity trader, String itemName, boolean toTrader) {
-        if (toTrader) {
-            final Iterator iterator = this.inventory.iterator();
-            while (iterator.hasNext()) {
-                AbstractItem item = (AbstractItem) iterator.next();
-                if (itemName.compareTo(item.getName()) == 0) { // Если нашли нужный предмет
-                    trader.giveItem(item);
-                    iterator.remove();
-                }
-            }
-        } else {
-            final Iterator iterator = trader.inventory.iterator();
-            while (iterator.hasNext()) {
-                AbstractItem item = (AbstractItem) iterator.next();
-                if (itemName.compareTo(item.getName()) == 0) { // Если нашли нужный предмет
-                    this.giveItem(item);
-                    iterator.remove();
-                }
-            }
-        }
-    }
-
+    
     @Override
     public void interract(Entity interractor) {
         System.out.println(interractor.getName() + " пнул(а) " + this.getName());

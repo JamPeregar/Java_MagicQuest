12842c2bd289a09fd149b4eb9590eb8095c73452
RinKirsan <116436137+RinKirsan@users.noreply.github.com>
2024-06-07 19:20:47 +0400

    Update SavedGame.java

--- src/classes/SavedGame.java
+++ src/classes/SavedGame.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class SavedGame implements Serializable {
+    private static final long serialVersionUID = 1L;
     private ArrayList<AbstractItem> saveInventory;
     public SavedGame(ArrayList<AbstractItem> saveInventory){
         this.saveInventory = saveInventory;
